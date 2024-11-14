f0861014eb3562d80c8c9d44e8b1fc8f8bf60a2a
Katokari <katokarix@gmail.com>
2024-11-14 16:40:11 +0300

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeatCount) {
+if (repeatCount == 0) {
+    return "";
+} else if (repeatCount < 0) {
+    return "ERROR";
+}
 
+let newString = "";
+for (let i = 1; i <= repeatCount; i++) newString += string;
+
+return newString;
 };
 
 // Do not edit below this line
