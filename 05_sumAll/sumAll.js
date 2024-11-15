71dd6a716a85963c788d6d3f22a286b58515ec9e
Katokari <katokarix@gmail.com>
2024-11-15 13:41:52 +0300

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numFirst, numEnd) {
+    let sum = 0;
+    let tmp;
 
+    if (!Number.isInteger(numFirst) || !Number.isInteger(numEnd) || numFirst < 0 || numEnd < 0) {
+        return "ERROR";
+    }
+
+    if (numFirst > numEnd) {
+        tmp = numFirst;
+        numFirst = numEnd;
+        numEnd = tmp;
+    }
+    
+    while (numEnd >= numFirst) {
+        sum += numFirst;
+        numFirst++;
+    }
+    
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
